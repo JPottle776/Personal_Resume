c77813098d33dbe42e964b8dd311a2e5c4d467d2
Unknown <JPottle776@west-mec.org>
2018-05-21 15:27:43 -0700

    Added files
    Added index.html and main javascript file nothing really done

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+/*  
+    
+    Author: Jamin Pottle    
+    Date: 5.21.18
+
+    file: main.js
+
+*/
+
+/* Screen Dimensions */
+var screenHeight = screen.height;
+var screenWidth = screen.width;
+
+/* Browser Dimensions */
+$browserHeight = window.innerHeight;
+$browserWidth = window.innerWidth;
+
+/* Main onload function */
+window.onload = function() {
+    console.log(screenHeight);
+    console.log(screenWidth);
+
+    console.log($browserHeight)
+    console.log($browserWidth);
+}
