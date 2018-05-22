359f3c7e92890b7637e3ead0636cdf9832e1c848
Jamin Pottle <JPottle776@west-mec.org>
2018-05-21 21:52:41 -0700

    Started page
    Started to add content to to the page like background image and headers for about me

--- javascript/main.js
+++ javascript/main.js
@@ -7,19 +7,40 @@
 
 */
 
-/* Screen Dimensions */
-var screenHeight = screen.height;
-var screenWidth = screen.width;
-
-/* Browser Dimensions */
-$browserHeight = window.innerHeight;
-$browserWidth = window.innerWidth;
-
 /* Main onload function */
 window.onload = function() {
+    /* Screen Dimensions */
+    var screenHeight = screen.height;
+    var screenWidth = screen.width;
+
+    /* Browser Dimensions */
+    $browserHeight = window.innerHeight;
+    $browserWidth = window.innerWidth;
+
     console.log(screenHeight);
     console.log(screenWidth);
 
-    console.log($browserHeight)
+    console.log($browserHeight);
     console.log($browserWidth);
+    $("#header").height($browserHeight);
+    $("#nameDiv").height($browserHeight);
+}
+
+window.onresize = function() {
+    /* Screen Dimensions */
+    var screenHeight = screen.height;
+    var screenWidth = screen.width;
+
+    /* Browser Dimensions */
+    $browserHeight = window.innerHeight;
+    $browserWidth = window.innerWidth;
+
+    console.log(screenHeight);
+    console.log(screenWidth);
+
+    console.log($browserHeight);
+    console.log($browserWidth);
+
+    $("#header").height($browserHeight);
+    $("#nameDiv").height($browserHeight);
 }
